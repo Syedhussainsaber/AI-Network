d7f5c9fc82b1d95324df7884f3e5f5c3fb3f1466
SyedHussainSaber <syedhussainsaber8@gmail.com>
2023-09-02 19:48:58 +0530

    tokenomics added

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -11,18 +11,18 @@ Tokenomics
 
 <div className="tokenomics-container text-white flex flex-col w-full justify-center items-center gap-8 md:gap-6">
 <div className="token-section flex flex-row w-full items-center p-2">
-    <h4 className='w-1/2 text-center text-3xl text-[#59C09D] sm:text-4xl md:w-1/2'>Total Suppy</h4>
-    <p className='w-1/2 text-center text-2xl'>2000Cr </p>
+    <h4 className='w-1/2 text-center text-2xl text-[#59C09D] sm:text-4xl md:w-1/2'>Total Suppy</h4>
+    <p className='w-1/2 text-center text-xl sm:text-2xl'>2000Cr </p>
 </div>
 
 <div className="token-section flex flex-row w-full items-center p-2">
-    <h4 className='w-1/2 text-center text-3xl text-[#59C09D] sm:text-4xl md:w-1/2'>Every event</h4>
-    <p className='w-1/2 text-center text-2xl'> 5% burn </p>
+    <h4 className='w-1/2 text-center text-2xl text-[#59C09D] sm:text-4xl md:w-1/2'>Every event</h4>
+    <p className='w-1/2 text-center text-xl sm:text-2xl'> 5% burn </p>
 </div>
 
 <div className="token-section  flex flex-row w-full items-center p-2">
-    <h4 className='w-1/2 text-center text-3xl text-[#59C09D] sm:text-4xl md:w-1/2'>Total Burn</h4>
-    <p className='w-1/2 text-center text-2xl'> 60% </p>
+    <h4 className='w-1/2 text-center text-2xl text-[#59C09D] sm:text-4xl md:w-1/2'>Total Burn</h4>
+    <p className='w-1/2 text-center text-xl sm:text-2xl'> 60% </p>
 </div>
 
 {/* <div className="token-section  flex flex-row w-full">
@@ -31,18 +31,18 @@ Tokenomics
 </div> */}
 
 <div className="token-section  flex flex-row w-full items-center p-2">
-    <h4 className='w-1/2 text-center text-3xl text-[#59C09D] sm:text-4xl md:w-1/2'>Airdrop</h4>
-    <p className='w-1/2 text-center text-2xl'>20%</p>
+    <h4 className='w-1/2 text-center text-2xl text-[#59C09D] sm:text-4xl md:w-1/2'>Airdrop</h4>
+    <p className='w-1/2 text-center text-xl sm:text-2xl'>20%</p>
 </div>
 
 <div className="token-section  flex flex-row w-full items-center p-2">
-    <h4 className='w-1/2 text-center text-3xl text-[#59C09D] sm:text-4xl md:w-1/2'>Honor holding</h4>
-    <p className='w-1/2 text-center text-2xl'>10%</p>
+    <h4 className='w-1/2 text-center text-2xl text-[#59C09D] sm:text-4xl md:w-1/2'>Honor holding</h4>
+    <p className='w-1/2 text-center text-xl sm:text-2xl'>10%</p>
 </div>
 
 <div className="token-section  flex flex-row w-full items-center p-2">
-    <h4 className='w-1/2 text-center text-3xl text-[#59C09D] sm:text-4xl  md:w-1/2'>Exchange</h4>
-    <p className='w-1/2 text-center text-2xl'>10%</p>
+    <h4 className='w-1/2 text-center text-2xl text-[#59C09D] sm:text-4xl  md:w-1/2'>Exchange</h4>
+    <p className='w-1/2 text-center text-xl sm:text-2xl'>10%</p>
 </div>
 </div>
   
